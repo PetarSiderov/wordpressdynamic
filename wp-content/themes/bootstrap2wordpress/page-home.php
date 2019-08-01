7329fa641fc57759ea16882c51d85a73ddd94b5c
petarsiderov <petar.siderov@avicenna4it.com>
2019-08-01 13:00:34 +0200

    merging

--- wp-content/themes/bootstrap2wordpress/page-home.php
+++ wp-content/themes/bootstrap2wordpress/page-home.php
@@ -49,13 +49,8 @@ get_header();
                                 <span><?php echo $prelaunch_price ?></span>
                             </div><!-- price -->
                             <div class="price">
-<<<<<<< HEAD
                                 <h4>Launch Price <small>Coming soon!</small> </h4>
                                 <span><?php echo $launch_price ?></span>
-=======
-                                <h4>Launch Price <small>Coming soon!!!!!</small> </h4>
-                                <span>$299</span>
->>>>>>> 63bf6a12cf4692858dea267dafd150c142d70e96
                             </div><!-- price -->
                             <div class="price">
                                 <h4>Final Price<small>Coming soon!</small> </h4>
